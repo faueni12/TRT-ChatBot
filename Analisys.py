a7ba96578a6842a63db241bac3ac00bd9033263b
Felipe <48134577+faueni12@users.noreply.github.com>
2020-07-23 14:37:59 -0300

    Add files via upload
    First commit

---
+++ Analisys.py
@@ -0,0 +1,54 @@
+""" Classe das funções de análise. Aqui pegará as palavras-chave,
+e as descrições que as conterem (apenas uma parte delas, de forma resumida)"""
+# -*- coding: UTF-8 -*- 
+import pandas as pd
+
+from nltk.probability import FreqDist
+from nltk.tokenize import word_tokenize
+
+import settings
+
+class Analisys:
+    def __init__(self, df):
+        self.df = df
+        self.df_tokens = {}
+
+    """ A função vai pegar todas as palavras mais frequentes, retornando
+    um DataFrame com o argumento "nameToSave"
+    """
+    def getFrequentTokens(self, quantity=150, **kwargs):
+        # Obter tokens da descrição
+        tokens = []
+        for frase in self.df.apply(lambda phrase: str.lower(str(phrase))):
+            tokens += word_tokenize(frase)
+
+        # Remover stopwords
+        tokens = [token for token in tokens if token not in settings.stop_words]
+
+        # Tokens mais frequentes
+        freqTokens = FreqDist(tokens).most_common(quantity)
+        
+        # Salvá-los em csv se tiver o argumento "nameToSave"
+        nameDf = kwargs.get("nameToSave")
+        if nameDf:
+            word = [w[0] for w in freqTokens]
+            quantity = [q[1] for q in freqTokens]
+            df = pd.DataFrame({'word':word, 'quantity':quantity})
+            df.to_csv(nameDf, encoding="utf-8")
+
+            # Guardar os dataFrames pra melhorar o desempenho da proxima função
+            self.df_tokens[nameDf] = df
+
+        return freqTokens
+
+    """ Pegará as descrições que tiverem um numero de ocorrencias de tokens """
+    def analizeDescription(self, description, nameDfTokens, occurrences, separator='.', quantity=150):
+        tokens = self.df_tokens[nameDfTokens]["word"][:quantity]
+        # Acessar cada frase separada por um separador (por padrão, "."),
+        # pra resumir as descrições. Vai pegar a primeira que tiver esse numero de ocorrencias
+        for descript in str.lower(description).split(separator):
+            # contar quantas ocorrencias de tokens na frase
+            occurrencesTokens = [descript.count(str(token)) for token in tokens]
+            qtTokens = len([occur for occur in occurrencesTokens if occur > 0])
+            if qtTokens >= occurrences:
+                return descript
---
+++ settings.py
@@ -0,0 +1,30 @@
+""" Configurações e constantes do projeto """
+# -*- coding: UTF-8 -*- 
+from nltk.corpus import stopwords
+
+# Lista de palavras não tão importantes pra análise
+stop_words = stopwords.words('portuguese')
+
+# Exceto essas
+stop_words.remove('não')
+stop_words.remove('esta')
+stop_words.remove('está')
+stop_words.remove('estão')
+
+stop_words += ['<', 'br', '>', 'nbsp', ',', '.', ';', '&', '(', ')', '!', "''", '``', 'bom', 'dia', 
+    'g', '-', 'vt', 'mat', 'nº', 'pois', ':', '@', 'ter', 'gentileza', 'boa', 'tarde', 'noite',
+    'obrigado', '?', 'juiz', 'hoje', 'secretaria', 'dra', 'urgencia', 'urgência', 'humberto',
+    'desta', '<span>', '</span>', '/span', 'span', 'ser', 'segue', 'gt', 'intuito', '--', 'div', '/div', 'obs',
+    '[',']', 'destinar', 'ronaldo', 'compor', 'após', 'segs', '2º', 'seres.', '*', 'conforme',
+    'atenciosamente', 'natal', 'equipe', 'escola', '2', 'segundo', 'vara', 'judicial', 'dois',
+    'apoio', 'encontra-se', '1', 'seres', 'fernades/ivia', 'ivia', 'distribuição', 
+    'encaminhar', 'coordenadoria', 'feita.', '/a', 'demais', 'tácio', 'usp=sharing', '3', '4',
+    'turma', 'possível', 'href', 'href=', 'urgente', 'presidência', 'usada', 'veio', 'encontra',
+    ]
+
+simbols = ['<br>', 'nbsp', '&', '&nbsp', '`', '@', '/a', 'href', 'href=', 'usp=sharing',
+ '<span>', '</span>', '[', ']', '&', '(', ')', '!', "''", '"' ,'``', '/a', 'div', '/div']
+
+# Ao tentar reduzir a frase tirando o ".", pode ser tirado o ".com" ".br", etc
+# Isso é pra diminuir a chance de retirar um "." sem ser no fim das frases.
+stop_ponto = ['http', 'www', '.com', '.net', '.co', '.org', '.br']
